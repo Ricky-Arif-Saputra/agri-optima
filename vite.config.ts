ee9615e667e073be4f053e0c3b0c458cade20371
Ricky-Arif-Saputra <rickysaputra12345168@gmail.com>
2026-04-27 10:34:12 +0700

    fix: install lucide-react and update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      // Kita paksa Vite untuk menyertakan firebase ke dalam bundle
-      external: [],
     },
   },
 });
