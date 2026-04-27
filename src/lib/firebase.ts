810ca36e8fe2a4c248d9d5ce0f2bc79963ad9504
Ricky-Arif-Saputra <rickysaputra12345168@gmail.com>
2026-04-27 17:47:39 +0700

    fix: add firebase configuration file

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Ganti data di bawah ini dengan data dari Console Firebase kamu
+// Jika belum punya, buat dulu di https://console.firebase.google.com/
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "agri-optima.firebaseapp.com",
+  projectId: "agri-optima",
+  storageBucket: "agri-optima.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef12345"
 };
 
 const app = initializeApp(firebaseConfig);
